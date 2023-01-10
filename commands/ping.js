54724b92be45aee3e3fe667dc770c4210d88126b
RachelEJ <rej3713@gmail.com>
2023-01-10 16:46:42 -0600

    Update ping.js
    Added permission check

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,8 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription('Replies with Pong!'),
+		.setDescription('Replies with Pong!')
+		.setDefaultMemberPermissions(294205270016),
 	async execute(client, interaction) {
 		await interaction.reply('Pong!');
 	},
